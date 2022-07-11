d421d7ceecef8a7b9b5585082b60671a2fcf1cae
Khemchand Ojha <ojha.akash876@gmail.com>
2022-07-11 16:10:27 +0530

    11/07/22

--- src/Redux/user/userActions.js
+++ src/Redux/user/userActions.js
@@ -1,51 +1,50 @@
-import axios from 'axios'
+import axios from 'axios';
 import {
   FETCH_USERS_REQUEST,
   FETCH_USERS_SUCCESS,
   FETCH_USERS_FAILURE
-} from './userTypes'
+} from './userTypes';
 
-const UrlPreFix = process.env.REACT_APP_APIURL
+const UrlPreFix = process.env.REACT_APP_APIURL;
 
 export const fetchUsers = () => {
   return dispatch => {
-    dispatch(fetchUsersRequest())
-    axios
-      .get(UrlPreFix + 'Motor/RTOcityDisplayOrderJson', {
-        headers: {
-          Authorization: 'Basic aUFuZEFwcDppQW5kQXBwQDIwMjI=$@!'
-        },
-        params: {
-          UserId: 0
-        }
-      })
+    dispatch(fetchUsersRequest());
+    axios(UrlPreFix + 'Motor/RTOcityDisplayOrderJson', {
+      headers: {
+        Authorization: 'Basic aUFuZEFwcDppQW5kQXBwQDIwMjI=$@!'
+      },
+      params: {
+        UserId: 0
+      }
+    })
       .then(response => {
-        const users = response.data.Response
-        dispatch(fetchUsersSuccess(users))
+        const users = response.data.Response;
+        dispatch(fetchUsersSuccess(users));
       })
       .catch(error => {
         // error.message is the error message
-        dispatch(fetchUsersFailure(error.message))
-      })
-  }
-}
+        dispatch(fetchUsersFailure(error.message));
+      });
+  };
+};
 
 export const fetchUsersRequest = () => {
   return {
     type: FETCH_USERS_REQUEST
-  }
-}
+  };
+};
 
 export const fetchUsersSuccess = users => {
   return {
     type: FETCH_USERS_SUCCESS,
     payload: users
-  }
-}
+  };
+};
 
 export const fetchUsersFailure = error => {
   return {
     type: FETCH_USERS_FAILURE,
     payload: error
-  }
-}
+  };
+};
